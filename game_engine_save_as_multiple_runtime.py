d72291fd8e9c5b4b43585eb800adbb3bd78d10b2
Manuel Bellersen <BellersenM@googlemail.com>
2013-05-06 18:35:51 +0200

    Works with Linux

--- game_engine_save_as_multiple_runtime.py
+++ game_engine_save_as_multiple_runtime.py
@@ -34,7 +34,6 @@ import bpy
 import os
 import sys
 import shutil
-import tempfile
 from bpy.props import *
 
 
@@ -42,195 +41,197 @@ class SaveAsMultipleRuntime(bpy.types.Operator):
 	bl_idname = "wm.save_as_multiple_runtime"
 	bl_label = "Save As Multiple Game Engine Runtime"
 	bl_options = {'REGISTER'}
-	
-	#if sys.platform == 'darwin':
-		# XXX, this line looks suspicious, could be done better?
-		#blender_bin_dir_darwin = '/' + os.path.join(*bpy.app.binary_path.split('/')[0:-4])
-		#ext = '.app'
-	#else:
-		#blender_bin_path = bpy.app.binary_path
-		#blender_bin_dir = os.path.dirname(blender_bin_path)
-		#ext = os.path.splitext(blender_bin_path)[-1].lower()
 
 	blender_version = str(bpy.app.version[0]) + "." + str(bpy.app.version[1]) + "." + str(bpy.app.version[2])
 	default_player_path = bpy.utils.script_paths()[1] + os.sep + blender_version
-	
+
 	player_url = "https://bitbucket.org/Urfoex/bge-exporter/get/" + blender_version + ".tar.gz"
 
 	blender_bin_dir_linux = default_player_path + os.sep + "linux_64" + os.sep
 	blender_bin_dir_windows = default_player_path + os.sep + "windows_64" + os.sep
-	blender_bin_dir_darwin = default_player_path + os.sep +"osx_64" + os.sep + "Blender" + os.sep
+	blender_bin_dir_darwin = default_player_path + os.sep + "osx_64" + os.sep + "Blender" + os.sep
 
 	default_player_path_linux = os.path.join(blender_bin_dir_linux, 'blenderplayer')
 	default_player_path_windows = os.path.join(blender_bin_dir_windows, 'blenderplayer.exe')
 	default_player_path_osx = os.path.join(blender_bin_dir_darwin, 'blenderplayer.app')
 
+	start_blend = default_player_path + os.sep + "start.blend"
+
 	filepath = StringProperty(
 			subtype='FILE_PATH',
 			)
-	#copy_python = BoolProperty(
-			#name="Copy Python",
-			#description="Copy bundle Python with the runtime",
-			#default=True,
-			#)
+	create_windows_runtime = BoolProperty(
+			name="Create executable for Windows",
+			description="Create executable for Windows",
+			default=True
+			)
+	create_linux_runtime = BoolProperty(
+			name="Create executable for Linux",
+			description="Create executable for Linux",
+			default=True
+			)
+	create_osx_runtime = BoolProperty(
+			name="Create executable for OSX",
+			description="Create executable for OSX",
+			default=True
+			)
 
-    
 	def execute(self, context):
 		import time
 		start_time = time.clock()
-		print("Saving runtime to %r" % self.filepath)
+		print("Saving runtime to", self.filepath)
 		self.get_player_files()
 		self.create_directories()
-		self.WriteRuntime(player_path=self.default_player_path_linux,
-					output_path=self.filepath + os.sep + "bin_linux_64",
-					copy_python=True,
-					#copy_python=self.copy_python,
-					copy_dlls=False,
-					target_os='LINUX',
-					report=self.report,
-					)
-		self.WriteRuntime(player_path=self.default_player_path_windows,
-					output_path=self.filepath + os.sep + "bin_windows_64",
-					copy_python=True,
-					#copy_python=self.copy_python,
-					copy_dlls=True,
-					target_os='WINDOWS',
-					report=self.report,
-					)
-		self.WriteRuntime(player_path=self.default_player_path_osx,
-					output_path=self.filepath + os.sep + "bin_osx_64",
-					copy_python=True,
-					#copy_python=self.copy_python,
-					copy_dlls=False,
-					target_os='OSX',
-					report=self.report,
-					)
-		
-		print("Finished in %.4fs" % (time.clock()-start_time))
+		self.write_runtimes()
+		print("Finished in %.4fs" % (time.clock() - start_time))
 		return {'FINISHED'}
 
 	def invoke(self, context, event):
 		if not self.filepath:
-			ext = '.app' if sys.platform == 'darwin' else os.path.splitext(bpy.app.binary_path)[-1]
-			self.filepath = bpy.path.ensure_ext(bpy.data.filepath, ext)
+			self.filepath = bpy.data.filepath[:-6]
 
 		wm = context.window_manager
 		wm.fileselect_add(self)
 		return {'RUNNING_MODAL'}
-	
+
 	def get_player_files(self):
 		print("Getting files from:", self.player_url)
 		print("Putting to:", self.default_player_path)
-	
+
 	def create_directories(self):
 		if not os.path.exists(self.filepath):
 			os.makedirs(self.filepath)
-        
-	def WriteAppleRuntime(self, player_path, output_path, copy_python):
-		# Enforce the extension
-		if not output_path.endswith('.app'):
-			output_path += '.app'
 
-		# Use the system's cp command to preserve some meta-data
-		os.system('cp -R "%s" "%s"' % (player_path, output_path))
-		
-		bpy.ops.wm.save_as_mainfile(filepath=os.path.join(output_path, "Contents" + os.sep + "Resources" + os.sep + "game.blend"),
-									relative_remap=False,
-									compress=False,
-									copy=True,
-									)
-		
-		# Python doesn't need to be copied for OS X since it's already inside blenderplayer.app
-		
-	def WriteRuntime(self, player_path, output_path, copy_python, copy_dlls, target_os, report=print):
+	def write_runtimes(self):
+		#if self.create_windows_runtime:
+			#self.write_windows_runtime()
+		if self.create_linux_runtime:
+			self.write_linux_runtime()
+		#if self.create_osx_runtime:
+			#self.write_osx_runtime()
+		self.write_blend()
+		self.write_python()
+
+	def player_exists(self, player_path):
+		return os.path.isfile(player_path) or (os.path.exists(player_path) and player_path.endswith('.app'))
+
+	def create_player(self, player_path, target_path):
 		import struct
 
-		# Check the paths
-		if not os.path.isfile(player_path) and not(os.path.exists(player_path) and player_path.endswith('.app')):
-			report({'ERROR'}, "The player could not be found! Runtime not saved")
-			return
-		
-		# Check if we're bundling a .app
-		if player_path.endswith('.app'):
-			self.WriteAppleRuntime(player_path, output_path, copy_python)
-			return
-			
-		# Enforce "exe" extension on Windows
-		if player_path.endswith('.exe') and not output_path.endswith('.exe'):
-			output_path += '.exe'
-		
 		# Get the player's binary and the offset for the blend
 		file = open(player_path, 'rb')
 		player_d = file.read()
 		offset = file.tell()
 		file.close()
-		
-		# Create a tmp blend file (Blenderplayer doesn't like compressed blends)
-		tempdir = tempfile.mkdtemp()
-		blend_path = os.path.join(tempdir, bpy.path.clean_name(output_path))
-		bpy.ops.wm.save_as_mainfile(filepath=blend_path,
-									relative_remap=False,
-									compress=False,
-									copy=True,
-									)
-		blend_path += '.blend'
-		
+
 		# Get the blend data
-		blend_file = open(blend_path, 'rb')
+		blend_file = open(self.start_blend, 'rb')
 		blend_d = blend_file.read()
 		blend_file.close()
 
-		# Get rid of the tmp blend, we're done with it
-		os.remove(blend_path)
-		os.rmdir(tempdir)
-		
 		# Create a new file for the bundled runtime
-		output = open(output_path, 'wb')
-		
+		output = open(target_path, 'wb')
+
 		# Write the player and blend data to the new runtime
 		print("Writing runtime...", end=" ")
 		output.write(player_d)
 		output.write(blend_d)
-		
+
 		# Store the offset (an int is 4 bytes, so we split it up into 4 bytes and save it)
-		output.write(struct.pack('B', (offset>>24)&0xFF))
-		output.write(struct.pack('B', (offset>>16)&0xFF))
-		output.write(struct.pack('B', (offset>>8)&0xFF))
-		output.write(struct.pack('B', (offset>>0)&0xFF))
-		
+		output.write(struct.pack('B', (offset >> 24) & 0xFF))
+		output.write(struct.pack('B', (offset >> 16) & 0xFF))
+		output.write(struct.pack('B', (offset >> 8) & 0xFF))
+		output.write(struct.pack('B', (offset >> 0) & 0xFF))
+
 		# Stuff for the runtime
 		output.write(b'BRUNTIME')
 		output.close()
-		
+
 		print("done")
-		
-		# Make the runtime executable on Linux
-		if target_os == 'LINUX':
-			os.chmod(output_path, 0o755)
-			
+
+	def copy_python(self, python_path):
 		## Copy bundled Python
-		blender_dir = self.blender_bin_dir_linux if target_os == 'LINUX' else self.blender_bin_dir_windows
-		runtime_dir = os.path.dirname(output_path)
-		
-		if copy_python:
-			print("Copying Python files...", end=" ")
-			src = os.path.join(blender_dir, bpy.app.version_string.split()[0])
-			dst = os.path.join(runtime_dir, bpy.app.version_string.split()[0])
-			print("from", src, "to", dst)
-			if os.path.exists(dst):
-				shutil.rmtree(dst)
-			shutil.copytree(src=src, dst=dst)
-			print("done")
+		print("Copying Python files...", end=" ")
+		src = os.path.join(python_path, bpy.app.version_string.split()[0])
+		dst = os.path.join(self.filepath, bpy.app.version_string.split()[0])
+		print("from", src, "to", dst)
+		if os.path.exists(dst):
+			shutil.rmtree(dst)
+		shutil.copytree(src=src, dst=dst)
+		print("done")
 
-		# And DLLs
-		if copy_dlls:
-			print("Copying DLLs...", end=" ")
-			for file in [i for i in os.listdir(blender_dir) if i.lower().endswith('.dll')]:
-				src = os.path.join(blender_dir, file)
-				dst = os.path.join(runtime_dir, file)
-				shutil.copy2(src, dst)
+	def copy_dll(self):
+		print("Copying DLLs...", end=" ")
+		for file in [i for i in os.listdir(self.blender_bin_dir_windows) if i.lower().endswith('.dll')]:
+			src = os.path.join(self.blender_bin_dir_windows, file)
+			dst = os.path.join(self.filepath, file)
+			shutil.copy2(src, dst)
+		print("done")
 
-			print("done")
+	def write_runtime(self, player_path, target_path, python_path):
+		if not self.player_exists(player_path):
+			print({'ERROR'}, "Could not find", player_path)
+			return
+		print("Player:", player_path)
+		print("Target:", target_path)
+		print("Python:", python_path)
+		self.create_player(player_path, target_path)
+		self.copy_python(python_path)
+
+	def write_linux_runtime(self):
+		target = self.filepath + os.sep + "TODO_linux_64.bin"
+		self.write_runtime(
+				player_path=self.default_player_path_linux,
+				target_path=target,
+				python_path=self.blender_bin_dir_linux
+				)
+		os.chmod(target, 0o755)
+
+	def write_windows_runtime(self):
+		self.write_runtime(
+				player_path=self.default_player_path_windows,
+				target_path=self.filepath + os.sep + "TODO_windows_64.exe",
+				python_path=self.blender_bin_dir_windows
+				)
+		self.copy_dll()
+
+	def write_osx_runtime(self):
+		player_path = self.default_player_path_osx
+		target_path = self.filepath + os.sep + "TODO_osx_64.app"
+		print("Player:", player_path)
+		print("Target:", target_path)
+		if not self.player_exists(player_path):
+			print({'ERROR'}, "Could not find", player_path)
+			return
+		shutil.copytree(src=player_path, dst=target_path)
+		bpy.ops.wm.save_as_mainfile(
+				filepath=os.path.join(target_path, "Contents" + os.sep + "Resources" + os.sep + "game.blend"),
+				relative_remap=False,
+				compress=False,
+				copy=True,
+				)
+
+	def write_blend(self):
+		blend_path = os.path.join(self.filepath, "game.blend")
+				#bpy.path.clean_name(output_path) + '.blend')
+		bpy.ops.wm.save_as_mainfile(
+				filepath=blend_path,
+				relative_remap=False,
+				compress=True,
+				copy=True,
+				)
+
+	def write_python(self):
+		game_blend_file = "game.blend"
+		python_text = """import bge
+
+def load_game_blend(cont):
+	bge.logic.startGame(\""""
+		python_text += game_blend_file
+		python_text += """\")"""
+		python_file = open(self.filepath + os.sep + "start_game.py", "w")
+		python_file.write(python_text)
+		python_file.close()
 
 
 def menu_func(self, context):
@@ -239,13 +240,11 @@ def menu_func(self, context):
 
 def register():
 	bpy.utils.register_module(__name__)
-
 	bpy.types.INFO_MT_file_export.append(menu_func)
 
 
 def unregister():
 	bpy.utils.unregister_module(__name__)
-
 	bpy.types.INFO_MT_file_export.remove(menu_func)
 
 
